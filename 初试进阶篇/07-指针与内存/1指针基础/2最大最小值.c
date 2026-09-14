22346b125779a0f84ed7c74986880b0a480b3f7e
kensh-ux <ssrszzk-life@outlook.com>
2026-09-14 13:52:30 +0800

    测试

---
+++ 初试进阶篇/07-指针与内存/1指针基础/2最大最小值.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void minmax(int a[],int len,int *min,int *max);
+
+int main()
+{
+    int a[] = {1,2,3,4,5,6,7,8,9,23,34,45,56,67,78,23,12};
+    int min,max;
+
+    minmax(a,sizeof(a)/sizeof(a[0]),&min,&max);
+
+    printf("min=%d,max=%d\n",min,max);
+    return 0;
+}
+
+void minmax(int a[],int len,int *min,int *max)
+{
+    int i;
+    *min = *max =a[0];
+
+    for(i=1; i<len; i++)
+    {
+        if(a[i] < *min)
+        {
+            *min = a[i];
+        }
+        if(a[i] > *max)
+        {
+            *max = a[i];
+        }
+    }
+}
